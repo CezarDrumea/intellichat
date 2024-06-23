ba4109cd828e63fd72db55ac94422f4a9528c1dd
Serafim Radu-Tataru <sradutataru@griddynamics.com>
2024-06-23 23:52:15 +0300

    Added the API Key for gpt

--- spring-ws-server/src/main/java/com/involveininnovation/chat/config/GenAIConfiguration.java
+++ spring-ws-server/src/main/java/com/involveininnovation/chat/config/GenAIConfiguration.java
@@ -11,7 +11,7 @@ public class GenAIConfiguration {
     @Bean
     public ChatLanguageModel chatLanguageModel() {
         OpenAiChatModel.OpenAiChatModelBuilder builder = OpenAiChatModel.builder()
-                .apiKey(System.getenv("OPENAI_API_KEY"));
+                .apiKey("sk-proj-BKk7YIJPiDAm5VsIxiO9T3BlbkFJa1nte1j4hiLvn3LRgOW7");
         return builder.build();
     }
 
